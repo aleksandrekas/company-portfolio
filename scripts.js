ba6f9ce69a8176e9b80a821a0e5dfc235ed9a632
aleksandrekas <154304980+aleksandrekas@users.noreply.github.com>
2024-12-22 23:09:00 +0400

    added carousel functionality on buttons only

--- scripts.js
+++ scripts.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 window.addEventListener('scroll',()=>{
     if(window.scrollY >=30){
         document.getElementById("header").classList.add('scrolled')
@@ -10,3 +5,51 @@ window.addEventListener('scroll',()=>{
         document.getElementById("header").classList.remove('scrolled')
     }
 })
+
+
+const leftBtn = document.getElementById("left")
+const rightBtn = document.getElementById("right")
+const sliders = document.querySelectorAll("#carousel")
+let index =  0
+
+
+if (sliders.length > 0) {
+    sliders[index].classList.add('carouselActive');
+}
+
+
+function changeCarousel(index) {
+    sliders.forEach((slide, i) => {
+        slide.classList.remove('carouselActive');
+        slide.classList.add('disabled');
+        if (i === index) {
+            
+            slide.classList.remove('disabled');
+            slide.classList.add('carouselActive');
+        }
+    });
+}
+
+
+
+console.log(index)
+
+
+
+rightBtn.addEventListener('click', () => {
+    if (index < sliders.length - 1) {
+        index += 1;
+    } else {
+        index = 0; 
+    }
+    changeCarousel(index);
+});
+
+leftBtn.addEventListener('click', () => {
+    if (index > 0) {
+        index -= 1;
+    } else {
+        index = sliders.length - 1;
+    }
+    changeCarousel(index);
+});
